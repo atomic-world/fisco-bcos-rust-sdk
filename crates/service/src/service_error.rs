2ff6b39b88131096dd0a9535d30bacffc9ca334b
Tom.Huang <hzlhu.dargon@gmail.com>
2021-07-09 13:29:08 +0800

    feat(service): remove dead code

--- crates/service/src/service_error.rs
+++ crates/service/src/service_error.rs
@@ -8,14 +8,8 @@ pub enum ServiceError {
     #[error("hyper::http::Error")]
     HyperHttpError(#[from] hyper::http::Error),
 
-    #[error("String::FromUtf8Error")]
-    FromUtf8Error(#[from] std::string::FromUtf8Error),
-
     #[error("serde_json::Error")]
     SerdeJsonError(#[from] serde_json::Error),
-
-    #[error("std::num::ParseIntError")]
-    ParseIntError(#[from] std::num::ParseIntError),
 
     #[error("fisco bcos service error")]
     FiscoBcosError {
