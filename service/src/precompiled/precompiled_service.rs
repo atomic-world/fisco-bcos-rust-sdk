789ba2e549b095e9e3b0342d0ed677708db70813
Tom.Huang <hzlhu.dargon@gmail.com>
2021-12-09 13:42:12 +0800

    feat(precompiled): update error codes

--- service/src/precompiled/precompiled_service.rs
+++ service/src/precompiled/precompiled_service.rs
@@ -34,22 +34,78 @@ pub enum PrecompiledServiceError {
     }
 }
 
-const PERMISSION_DENIED: i32 = -50000;
-const TABLE_EXIST: i32 = -50001;
-const TABLE_NAME_AND_ADDRESS_NOT_EXIST: i32 = -51001;
-const TABLE_NAME_EXCEEDS: i32 = -51002;
-const CONTRACT_NOT_EXIST: i32 = -51003;
-const SPECIAL_PERMISSIONS: i32 = -51004;
-const TABLE_NAME_AND_ADDRESS_EXIST: i32 = -51009;
-const INVALID_NODE_ID: i32 = -51100;
+// ChainGovernancePrecompiled
+const CURRENT_VALUE_IS_EXPECTED_VALUE: i32 = -52012;
+const ACCOUNT_FROZEN: i32 = -52011;
+const ACCOUNT_ALREADY_AVAILABLE: i32 = -52010;
+const INVALID_ACCOUNT_ADDRESS: i32 = -52009;
+const ACCOUNT_NOT_EXIST: i32 = -52008;
+const OPERATOR_NOT_EXIST: i32 = -52007;
+const OPERATOR_EXIST: i32 = -52006;
+const COMMITTEE_MEMBER_CANNOT_BE_OPERATOR: i32 = -52005;
+const OPERATOR_CANNOT_BE_COMMITTEE_MEMBER: i32 = -52004;
+const INVALID_THRESHOLD: i32 = -52003;
+const INVALID_REQUEST_PERMISSION_DENIED: i32 = -52002;
+const COMMITTEE_MEMBER_NOT_EXIST: i32 = -52001;
+const COMMITTEE_MEMBER_EXIST: i32 = -52000;
+
+// ContractLifeCyclePrecompiled
+const INVALID_REVOKE_LAST_AUTHORIZATION: i32 = -51907;
+const INVALID_NON_EXIST_AUTHORIZATION: i32 = -51906;
+const INVALID_NO_AUTHORIZED: i32 = -51905;
+const INVALID_TABLE_NOT_EXIST: i32 = -51904;
+const INVALID_CONTRACT_ADDRESS: i32 = -51903;
+const INVALID_CONTRACT_REPEAT_AUTHORIZATION: i32 = -51902;
+const INVALID_CONTRACT_AVAILABLE: i32 = -51901;
+const INVALID_CONTRACT_FROZEN: i32 = -51900;
+
+// RingSigPrecompiled
+const VERIFY_RING_SIG_FAILED: i32 = -51800;
+
+// GroupSigPrecompiled
+const VERIFY_GROUP_SIG_FAILED: i32 = -51700;
+
+// PaillierPrecompiled
+const INVALID_CIPHERS: i32 = -51600;
+
+// CRUDPrecompiled
+const INVALID_UPDATE_TABLE_KEY: i32 = -51503;
+const CONDITION_OPERATION_UNDEFINED: i32 = -51502;
+const PARSE_CONDITION_ERROR: i32 = -51501;
+const PARSE_ENTRY_ERROR: i32 = -51500;
+
+// SystemConfigPrecompiled
+const INVALID_CONFIGURATION_VALUES: i32 = -51300;
+
+// CNSPrecompiled
+const VERSION_LENGTH_OVERFLOW: i32 = -51201;
+const ADDRESS_AND_VERSION_EXIST: i32 = -51200;
+
+// ConsensusPrecompiled
 const LAST_SEALER: i32 = -51101;
-const P2P_NETWORK: i32 = -51102;
-const GROUP_PEERS: i32 = -51103;
-const SEALER_LIST: i32 = -51104;
-const OBSERVER_LIST: i32 = -51105;
-const VERSION_EXCEEDS: i32 = -51200;
-const CONTRACT_NAME_AND_VERSION_EXIST: i32 = -51201;
-const INVALID_INPUT: i32 = -51300;
+const INVALID_NODE_ID: i32 = -51100;
+
+// PermissionPrecompiled
+const COMMITTEE_PERMISSION: i32 = -51004;
+const CONTRACT_NOT_EXIST: i32 = -51003;
+const TABLE_NAME_OVERFLOW: i32 = -51002;
+const TABLE_AND_ADDRESS_NOT_EXIST: i32 = -51001;
+const TABLE_AND_ADDRESS_EXIST: i32 = -51000;
+
+// Common error code among all precompiled contracts
+const ADDRESS_INVALID: i32 = -50102;
+const UNKNOWN_FUNCTION_CALL: i32 = -50101;
+const TABLE_NOT_EXIST: i32 = -50100;
+
+const NO_AUTHORIZED: i32 = -50000;
+const TABLE_NAME_ALREADY_EXIST: i32 = -50001;
+const TABLE_NAME_LENGTH_OVERFLOW: i32 = -50002;
+const TABLE_FILED_LENGTH_OVERFLOW: i32 = -50003;
+const TABLE_FILED_TOTAL_LENGTH_OVERFLOW: i32 = -50004;
+const TABLE_KEY_VALUE_LENGTH_OVERFLOW: i32 = -50005;
+const TABLE_FIELD_VALUE_LENGTH_OVERFLOW: i32 = -50006;
+const TABLE_DUPLICATE_FIELD: i32 = -50007;
+const TABLE_INVALIDATE_FIELD: i32 = -50008;
 
 pub(crate) fn parse_output(output: &str) -> Result<i32, PrecompiledServiceError> {
     let data = hex::decode(output.to_owned().trim_start_matches("0x").as_bytes())?;
@@ -60,22 +116,56 @@ pub(crate) fn parse_output(output: &str) -> Result<i32, PrecompiledServiceError>
     }
 
     let message = match code {
-        PERMISSION_DENIED => "Permission denied".to_owned(),
-        TABLE_EXIST => "Table name already exist".to_owned(),
-        TABLE_NAME_AND_ADDRESS_NOT_EXIST => "Table name and address does not exist".to_owned(),
-        TABLE_NAME_EXCEEDS => "The length of the table name exceeds the maximum limit".to_owned(),
-        CONTRACT_NOT_EXIST => "Contract does not exist".to_owned(),
-        SPECIAL_PERMISSIONS => "Special permissions for ChainGovernancePrecompiled committee".to_owned(),
-        TABLE_NAME_AND_ADDRESS_EXIST => "Table name and address already exist".to_owned(),
-        INVALID_NODE_ID => "Invalid node ID".to_owned(),
+        CURRENT_VALUE_IS_EXPECTED_VALUE => "The current value is expected".to_owned(),
+        ACCOUNT_FROZEN => "The account is frozen".to_owned(),
+        ACCOUNT_ALREADY_AVAILABLE => "The account is already available".to_owned(),
+        INVALID_ACCOUNT_ADDRESS => "Invalid account address".to_owned(),
+        ACCOUNT_NOT_EXIST => "Account not exist, you can create a blockchain account by using this account to deploy contracts on the chain".to_owned(),
+        OPERATOR_NOT_EXIST => "The operator not exist".to_owned(),
+        OPERATOR_EXIST => "The operator already exist".to_owned(),
+        COMMITTEE_MEMBER_CANNOT_BE_OPERATOR => "The committee member cannot be operator".to_owned(),
+        OPERATOR_CANNOT_BE_COMMITTEE_MEMBER => "The operator or cnsManager cannot be committee member".to_owned(),
+        INVALID_THRESHOLD => "Invalid threshold, threshold should from 0 to 99".to_owned(),
+        INVALID_REQUEST_PERMISSION_DENIED => "Invalid request for permission deny".to_owned(),
+        COMMITTEE_MEMBER_NOT_EXIST => "The committee member not exist".to_owned(),
+        COMMITTEE_MEMBER_EXIST => "The committee member already exist".to_owned(),
+        INVALID_REVOKE_LAST_AUTHORIZATION => "There is only one contract status manager left, and the revoke operation cannot be performed".to_owned(),
+        INVALID_NON_EXIST_AUTHORIZATION => "The contract status manager doesn't exist".to_owned(),
+        INVALID_NO_AUTHORIZED => "Have no permission to access the contract table".to_owned(),
+        INVALID_TABLE_NOT_EXIST => "The queried contract address doesn't exist".to_owned(),
+        INVALID_CONTRACT_ADDRESS => "The contract address is invalid".to_owned(),
+        INVALID_CONTRACT_REPEAT_AUTHORIZATION => "The contract has been granted authorization with same user".to_owned(),
+        INVALID_CONTRACT_AVAILABLE => "The contract is available".to_owned(),
+        INVALID_CONTRACT_FROZEN => "The contract has been frozen".to_owned(),
+        VERIFY_RING_SIG_FAILED => "Verify ring signature failed".to_owned(),
+        VERIFY_GROUP_SIG_FAILED => "Verify group signature failed".to_owned(),
+        INVALID_CIPHERS => "Execute PaillierAdd failed".to_owned(),
+        INVALID_UPDATE_TABLE_KEY => "Don't update the table key".to_owned(),
+        CONDITION_OPERATION_UNDEFINED => "Undefined function of Condition Precompiled".to_owned(),
+        PARSE_CONDITION_ERROR => "Parse the input of Condition Precompiled failed".to_owned(),
+        PARSE_ENTRY_ERROR => "Parse the input of the Entry Precompiled failed".to_owned(),
+        INVALID_CONFIGURATION_VALUES => "Invalid configuration entry".to_owned(),
+        VERSION_LENGTH_OVERFLOW => "The version string length exceeds the maximum limit".to_owned(),
+        ADDRESS_AND_VERSION_EXIST => "The contract name and version already exist".to_owned(),
         LAST_SEALER => "The last sealer cannot be removed".to_owned(),
-        P2P_NETWORK => "The node is not reachable".to_owned(),
-        GROUP_PEERS => "The node is not a group peer".to_owned(),
-        SEALER_LIST => "The node is already in the sealer list".to_owned(),
-        OBSERVER_LIST => "The node is already in the observer list".to_owned(),
-        VERSION_EXCEEDS => "Version string length exceeds the maximum limit".to_owned(),
-        CONTRACT_NAME_AND_VERSION_EXIST => "Contract name and version already exist".to_owned(),
-        INVALID_INPUT => "Invalid input".to_owned(),
+        INVALID_NODE_ID => "Invalid node ID".to_owned(),
+        COMMITTEE_PERMISSION => "The committee permission control by ChainGovernancePrecompiled are recommended".to_owned(),
+        CONTRACT_NOT_EXIST => "The contract doesn't exist".to_owned(),
+        TABLE_NAME_OVERFLOW => "The table name string length exceeds the maximum limit".to_owned(),
+        TABLE_AND_ADDRESS_NOT_EXIST => "The table name and address not exist".to_owned(),
+        TABLE_AND_ADDRESS_EXIST => "The table name and address already exist".to_owned(),
+        ADDRESS_INVALID => "Invalid address format".to_owned(),
+        UNKNOWN_FUNCTION_CALL => "Undefined function".to_owned(),
+        TABLE_NOT_EXIST => "Open table failed, please check the existence of the table".to_owned(),
+        NO_AUTHORIZED => "Permission denied".to_owned(),
+        TABLE_NAME_ALREADY_EXIST => "The table already exist".to_owned(),
+        TABLE_NAME_LENGTH_OVERFLOW => "The table name length exceeds the limit 48".to_owned(),
+        TABLE_FILED_LENGTH_OVERFLOW => "The table field name exceeds the limit 64".to_owned(),
+        TABLE_FILED_TOTAL_LENGTH_OVERFLOW => "The length of all the fields name exceeds the limit 1024".to_owned(),
+        TABLE_KEY_VALUE_LENGTH_OVERFLOW => "The value exceeds the limit, key max length is 255, field value max length is 1024".to_owned(),
+        TABLE_FIELD_VALUE_LENGTH_OVERFLOW => "The field value exceeds the limit 1024".to_owned(),
+        TABLE_DUPLICATE_FIELD => "The table contains duplicated field".to_owned(),
+        TABLE_INVALIDATE_FIELD => "Invalid table name or field name".to_owned(),
         _ => format!("unknown output code:{:?}", code)
     };
     Err(PrecompiledServiceError::FiscoBcosError { code, message })
